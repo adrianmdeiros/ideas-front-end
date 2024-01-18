426cb20ba80610332a9ee13108c319576a8d045f
Adrian Medeiros <lima.adrian13@gmail.com>
2024-01-18 19:35:07 -0300

    vLibras library added by conventional way to fix its use in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import { Outlet } from "react-router-dom";
 import GlobalStyle from "./styles/global";
 import AuthProvider from "./contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
-import VLibras from "@djpfs/react-vlibras";
 
 
 
 function App() {
   return (
     <>
-    <Toaster position="bottom-right"/>
+    <Toaster position="top-center"/>
     <GlobalStyle />
-    <VLibras />
     <AuthProvider> 
       <Outlet />
     </AuthProvider>
